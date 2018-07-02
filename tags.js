f80e738fe1f6a2c33a10a37516044304242d6ca4
Justin Meyer <justinbmeyer@gmail.com>
2018-07-01 20:51:09 -0500

    can copy tags

--- tags.js
+++ tags.js
@@ -6,8 +6,8 @@ exports.codepen = {
         var useCurData = validCurData && (typeof curData.description === "string") && !curData.body;
 
         // copies codepen options on to the docObject so they are accessible by the script
-        if(options.codepen) {
-            this.codepen = options.codepen;
+        if(options.siteConfig.codepen) {
+            this.codepen = options.siteConfig.codepen;
         }
 
         if(useCurData) {
